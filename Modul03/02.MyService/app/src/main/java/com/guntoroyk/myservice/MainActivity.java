00092e8562bfe79908e7fb8bda346af4fab51282
igun369 <guntoro.gyk@gmail.com>
2019-07-28 16:30:27 +0700

    add BoundService on 02.MyService

--- Modul03/02.MyService/app/src/main/java/com/guntoroyk/myservice/MainActivity.java
+++ Modul03/02.MyService/app/src/main/java/com/guntoroyk/myservice/MainActivity.java
@@ -1,6 +1,9 @@
 package com.guntoroyk.myservice;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +14,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnStartIntentService;
     Button btnStartBoundService;
     Button btnStopBoundService;
+
+    boolean mServiceBound = false;
+    BoundService mBoundService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mServiceBound) {
+            unbindService(mServiceConnection);
+        }
+    }
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_start_service:
@@ -43,13 +57,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startService(mStartIntentService);
                 break;
             case R.id.btn_start_bound_service:
-
+                Intent mBoundServiceIntent = new Intent(MainActivity.this, BoundService.class);
+                bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
                 break;
             case R.id.btn_stop_bound_service:
-
+                unbindService(mServiceConnection);
                 break;
-
         }
 
     }
+
+
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            BoundService.MyBinder myBinder = (BoundService.MyBinder) service;
+            mBoundService = myBinder.getService();
+            mServiceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
+    };
 }
